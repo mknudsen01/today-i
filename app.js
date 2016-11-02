6f77fafaed8347ac67486bef949d5ae2fc779dfd
Matthew Knudsen <mknudsen01@gmail.com>
2016-11-02 13:13:55 -0700

    eslint --fix app.js fixes

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
 // grab the activities from local storage
-var activityByDay = localStorage.getItem('activityByDay') ? JSON.parse(localStorage.getItem('activityByDay')) : {};
-var activitiesHolder = document.querySelector('#activities');
+const activityByDay = localStorage.getItem('activityByDay') ? JSON.parse(localStorage.getItem('activityByDay')) : {};
+const activitiesHolder = document.querySelector('#activities');
 
 // paint activities on initial page load
 paintActivities();
 
 function clearActivitiesHolder() {
   activitiesHolder.innerHTML = '';
-};
+}
 
 function paintActivities() {
-  for (var day in activityByDay) {
-    var activities = activityByDay[day];
-    var dateHolder = document.createElement("h3");
-    var eventDate = new Date(parseInt(day));
-    var dateOptions = { year: "numeric", month: "long", day: "numeric" };
+  for (const day in activityByDay) {
+    const activities = activityByDay[day];
+    const dateHolder = document.createElement('h3');
+    const eventDate = new Date(parseInt(day));
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
     activitiesHolder.appendChild(dateHolder);
-    dateHolder.innerHTML = eventDate.toLocaleDateString("en-US", dateOptions);
+    dateHolder.innerHTML = eventDate.toLocaleDateString('en-US', dateOptions);
 
-    for (var i = 0, activitiesLength = activities.length; i < activitiesLength; i++) {
-      var activity = activities[i];
-      var eventTemplate = document.querySelector("#activity-template").cloneNode(true).innerHTML;
-      populatedTemplate = eventTemplate.replace("{{activity}}", activity);
+    for (let i = 0, activitiesLength = activities.length; i < activitiesLength; i++) {
+      const activity = activities[i];
+      const eventTemplate = document.querySelector('#activity-template').cloneNode(true).innerHTML;
+      populatedTemplate = eventTemplate.replace('{{activity}}', activity);
 
-      var eventTextHolder = document.createElement("div");
+      const eventTextHolder = document.createElement('div');
       eventTextHolder.innerHTML = populatedTemplate;
       activitiesHolder.appendChild(eventTextHolder);
-      eventTextHolder.setAttribute("id", "activity");
-      eventTextHolder.setAttribute("class", "grid grid--center");
+      eventTextHolder.setAttribute('id', 'activity');
+      eventTextHolder.setAttribute('class', 'grid grid--center');
     }
   }
-};
+}
 
-document.querySelector("#activity-form").addEventListener('submit', function() {
+document.querySelector('#activity-form').addEventListener('submit', () => {
   // prevent form submission/page reload
   event.preventDefault();
 
   // save the current date (start of the day)
-  var now = new Date();
-  var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  var startOfDayTimestamp = startOfDay.getTime();
+  const now = new Date();
+  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const startOfDayTimestamp = startOfDay.getTime();
 
   // grab text input's value
-  var textInput = document.querySelector('input[name="activity"]');
+  const textInput = document.querySelector('input[name="activity"]');
 
-  var activityContent = textInput.value;
+  const activityContent = textInput.value;
   textInput.value = '';
 
   if (!activityContent) {
     return;
   }
-
 
 
   // add activity and date to our activities collection, which is now in localStorage
@@ -66,4 +65,4 @@ document.querySelector("#activity-form").addEventListener('submit', function() {
 
   // loop through activity collection and write a paragraph for each one in the list
   paintActivities();
-})
+});
