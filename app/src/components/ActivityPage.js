ff4af579edcd1dc3a7bafca4c78d1375244383ac
Matthew Knudsen <mknudsen01@gmail.com>
2016-11-08 12:41:46 -0800

    Add markup for app/header/home/activity form/page

---
+++ app/src/components/ActivityPage.js
@@ -0,0 +1,48 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
+
+import ActivityItem from '~/components/ActivityItem';
+import NewActivityForm from '~/containers/NewActivityForm';
+
+import StorageService from '~/utils/StorageService';
+
+
+function ActivityPage(props) {
+  const { activityByDay } = props;
+
+  // set the activities we're about to render to local storage
+  StorageService.set('activityByDay', activityByDay);
+
+  const items = Object.keys(activityByDay).map(timestamp =>
+    <ActivityItem timestamp={timestamp} activities={activityByDay[timestamp]} />
+  );
+  return (
+    <div className="grid mt">
+      <Helmet title={`Activities`} />
+      <NewActivityForm />
+      <div className="grid__item col-1-1">
+        {items}
+      </div>
+    </div>
+  );
+}
+
+ActivityPage.propTypes = {
+  activityByDay: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    activityByDay: state.activityByDay,
+  };
+}
+
+function mapDispatchToProps() {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ActivityPage);
---
+++ app/src/containers/NewActivityForm.js
@@ -0,0 +1,69 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import DateService from '~/utils/DateService';
+
+import * as activityActions from '~/actions/activities';
+
+class NewActivityForm extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.addActivity = this.addActivity.bind(this);
+  }
+
+  componentDidMount() {
+    this.activityInput.value = '';
+  }
+
+  addActivity(e) {
+    e.preventDefault();
+    const today = DateService.todayUnix();
+    this.props.actions.addActivity({ timestamp: today, activity: this.activityInput.value.trim() });
+    this.activityInput.value = '';
+  }
+
+  render() {
+    return (
+      <form
+        autoComplete="off"
+        className="grid grid--center"
+        onSubmit={this.addActivity}
+      >
+        <div className="grid__item grid__item--3-4">
+          <input
+            type="text"
+            ref={(ref) => { this.activityInput = ref; }}
+            name="activity"
+            placeholder="What did you do?"
+            className="w--1-1 font--11-10 p-"
+          />
+        </div>
+        <div className="grid__item grid__item--1-4">
+          <button className="w--1-1 font--11-10 pv-">Save</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+NewActivityForm.propTypes = {
+  actions: PropTypes.object,
+};
+
+function mapStateToProps() {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(activityActions, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NewActivityForm);
