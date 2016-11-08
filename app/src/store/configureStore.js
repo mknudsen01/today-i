51b3c9e509739d23dcda648bc4c05b42a43af032
Matthew Knudsen <mknudsen01@gmail.com>
2016-11-08 12:34:21 -0800

    Add configure store file for dev

---
+++ app/src/store/configureStore.js
@@ -0,0 +1,6 @@
+module.exports = require('./configureStore.dev');
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./configureStore.prod');
+// } else {
+//   module.exports = require('./configureStore.dev');
+// }
