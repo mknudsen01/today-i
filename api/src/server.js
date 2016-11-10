38a3ed05b465e6c245d13d48734024c3c00002c3
Matthew Knudsen <mknudsen01@gmail.com>
2016-11-10 13:35:59 -0800

    Add server with a couple of loggers

---
+++ api/src/server.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+const koa = require('koa');
+const KoaBody = require('koa-body');
+const Router = require('koa-router');
+const db = require('./config/db.js');
+const env = require('./config/env');
+
+const app = koa();
+const router = new Router();
+const koaBody = new KoaBody();
+const PORT = env.PORT;
+
+app.context.db = db;
+
+
+
+// x-response-time
+
+app.use(function* xResponseTime(next) {
+  const start = new Date();
+  yield next;
+  const ms = new Date() - start;
+  this.set('X-Response-Time', `${ms} ms`);
+});
+
+// logger
+app.use(function* logger(next) {
+  const start = new Date();
+  yield next;
+  const ms = new Date() - start;
+  console.log('%s %s - %s ms', this.method, this.url, ms);
+});
+
+app.use(function* catchAll(next) {
+  this.body = 'hello from koa';
+  yield next;
+});
+
+app.use(router.routes());
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log('Express listening on port:', PORT);
+  });
+});
+
+/* eslint-enable no-console */
