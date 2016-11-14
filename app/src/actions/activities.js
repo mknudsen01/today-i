4b9be6cfaba3c75a78d2d6400f592ba6df09276c
Matthew Knudsen <mknudsen01@gmail.com>
2016-11-14 11:57:58 -0800

    Add activities action to fetch activities from server

--- app/src/actions/activities.js
+++ app/src/actions/activities.js
@@ -1,7 +1,22 @@
+import ApiService from '~/utils/ApiService';
+
 export const ADD_ACTIVITY = 'ADD_ACTIVITY';
+export const RECEIVE_ACTIVITIES = 'RECEIVE_ACTIVITIES';
 
 export const addActivity = ({ activity, timestamp }) => ({
   type: ADD_ACTIVITY,
   activity,
   timestamp,
 });
+
+export const receiveActivities = activities => ({
+  type: RECEIVE_ACTIVITIES,
+  activities,
+});
+
+export const getActivities = () => dispatch =>
+  ApiService.get(`/activities`)
+  .then((res) => {
+    dispatch(receiveActivities(res));
+  });
+
